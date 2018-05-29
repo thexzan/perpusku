efda4a1ed150019aa3f190172a59cae70c8c84aa
Xzan <hello@thexzan.com>
2018-05-29 23:33:41 +0700

    buku - add ent , imp, int

---
+++ entity/ent_buku.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author xzan
+ */
+public class ent_buku {
+    private int id;
+    private String judul;
+    private String kategori;
+    private String penerbit;
+    private int stok;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public String getPenerbit() {
+        return penerbit;
+    }
+
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+    
+    
+}
---
+++ interfaces/int_buku.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+import entity.ent_buku;
+import java.util.List;
+
+/**
+ *
+ * @author xzan
+ */
+public interface int_buku {
+    boolean insertBuku (ent_buku b);
+    boolean updateBuku (ent_buku b);
+    boolean deleteBuku (int id);
+    List getBuku(String cari);
+}
