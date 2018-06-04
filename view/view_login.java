25bfbfafbc336c2f1d47613de4952a60f0da5c0f
Xzan <hello@thexzan.com>
2018-06-04 23:48:02 +0700

    animation on login form

--- view/view_login.java
+++ view/view_login.java
@@ -27,7 +27,7 @@ public class view_login extends javax.swing.JFrame {
     private final int_login loginDAO;
     private ent_login x;
 
-    private void do_login(){
+    private void do_login() {
         x = new ent_login();
         x.setUsername(username.getText());
         x.setPassword(password.getText());
@@ -41,7 +41,7 @@ public class view_login extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Username / Password Tidak Cocok!");
         }
     }
-    
+
     public view_login() {
         initComponents();
         setLocationRelativeTo(null);
@@ -56,6 +56,7 @@ public class view_login extends javax.swing.JFrame {
         };
 
         password.addActionListener(action);
+        btn_login.setVisible(false);
     }
 
     /**
@@ -91,12 +92,17 @@ public class view_login extends javax.swing.JFrame {
                 btn_loginMouseEntered(evt);
             }
         });
-        getContentPane().add(btn_login, new org.netbeans.lib.awtextra.AbsoluteConstraints(169, 318, 60, -1));
+        getContentPane().add(btn_login, new org.netbeans.lib.awtextra.AbsoluteConstraints(167, 320, 60, -1));
 
         password.setBackground(new Color(0,0,0,0));
         password.setFont(new java.awt.Font("Osaka", 0, 20)); // NOI18N
         password.setForeground(new java.awt.Color(0, 0, 0));
         password.setBorder(null);
+        password.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                passwordFocusGained(evt);
+            }
+        });
         getContentPane().add(password, new org.netbeans.lib.awtextra.AbsoluteConstraints(88, 269, 220, 30));
 
         username.setBackground(new Color(0,0,0,0));
@@ -105,9 +111,14 @@ public class view_login extends javax.swing.JFrame {
         username.setBorder(null);
         username.setSelectedTextColor(new java.awt.Color(255, 255, 255));
         username.setSelectionColor(new java.awt.Color(0, 122, 255));
+        username.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                usernameFocusGained(evt);
+            }
+        });
         getContentPane().add(username, new org.netbeans.lib.awtextra.AbsoluteConstraints(88, 199, 220, 30));
 
-        bg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/bg_login.png"))); // NOI18N
+        bg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/bg_login.gif"))); // NOI18N
         getContentPane().add(bg, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
@@ -124,6 +135,14 @@ public class view_login extends javax.swing.JFrame {
     private void btn_loginMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_loginMouseClicked
         do_login();
     }//GEN-LAST:event_btn_loginMouseClicked
+
+    private void usernameFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_usernameFocusGained
+        // TODO add your handling code here:
+    }//GEN-LAST:event_usernameFocusGained
+
+    private void passwordFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_passwordFocusGained
+        btn_login.setVisible(true);
+    }//GEN-LAST:event_passwordFocusGained
 
     /**
      * @param args the command line arguments
